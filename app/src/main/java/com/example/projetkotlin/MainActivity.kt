a207974d7131ead6791fdfe90feb5b24879b2bd4
Patey Valentin <valentin.patey@etu.unicaen.fr>
2024-03-30 15:11:18 +0100

    fix: removed unused imports and useless () or ;

--- app/src/main/java/com/example/projetkotlin/MainActivity.kt
+++ app/src/main/java/com/example/projetkotlin/MainActivity.kt
@@ -6,32 +6,21 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
@@ -55,9 +44,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Column(modifier = Modifier.fillMaxSize()) {
                         Test()
-
                         val apiRequest: APIClient by viewModels()
-
                         Accueil(apiRequest)
                     }
                 }
@@ -69,8 +56,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Test() {
-    val context = LocalContext.current;
-    Column() {
+    val context = LocalContext.current
+    Column {
         Button(onClick = {
             context.startActivity(Intent(context, ViewActivity::class.java))
         }) {
