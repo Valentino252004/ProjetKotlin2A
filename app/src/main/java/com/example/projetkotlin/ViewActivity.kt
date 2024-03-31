a100765110afbd1f528abeea90a333dae65abc33
lesieur221_perso <theo.lesieur@etu.unicaen.fr>
2024-03-31 15:18:09 +0200

    Commit to merge

--- app/src/main/java/com/example/projetkotlin/ViewActivity.kt
+++ app/src/main/java/com/example/projetkotlin/ViewActivity.kt
@@ -10,7 +10,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.RecyclerView
 
 class ViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
